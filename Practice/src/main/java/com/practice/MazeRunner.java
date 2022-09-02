b91a488421be7fae7f9d7e181987bab88637721f
gitmeg22 <35379612+gitmeg22@users.noreply.github.com>
2022-09-02 15:18:31 -0400

    Add main

---
+++ Practice/src/main/java/com/practice/MazeRunner.java
@@ -0,0 +1,22 @@
+package com.practice;
+
+public class MazeRunner {
+    public static void main(String[] args) {
+        int [][] data = {
+                {4, 6, 3, 10},
+                {4, 2, 40, 1, 7, 99, 100},
+                {5, 34, 1, 43}
+        };
+        data[1][2] = 49;
+        System.out.println(data[1][2]);
+
+        for (int i = 0; i < data.length; i++) {
+            System.out.println("row index " + i);
+            for (int j = 0; j < data[i].length; j++) {
+                System.out.println(data[i][j]);
+
+            }
+
+        }
+    }
+}
