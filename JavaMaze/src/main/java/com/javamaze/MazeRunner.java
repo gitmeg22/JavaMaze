c293896e3e2d6b65c319696abb71394356dd1e80
gitmeg22 <35379612+gitmeg22@users.noreply.github.com>
2022-09-05 15:21:37 -0400

    Create Position class; Test path.peek()

--- JavaMaze/src/main/java/com/javamaze/MazeRunner.java
+++ JavaMaze/src/main/java/com/javamaze/MazeRunner.java
@@ -1,11 +1,14 @@
 package com.javamaze;
 
+import java.util.LinkedList;
+
+
 public class MazeRunner {
     /* Store maze in program using static;
     * Static makes it accessible to the main method;
     * If maze was not static,
     * would need instance of maze in main */
-    int [][] maze = {
+    static int [][] maze = {
             {2, 0, 1, 1},
             {1, 1, 0, 1},
             {0, 1, 1, 1}
@@ -15,22 +18,26 @@ public class MazeRunner {
     // 1 = path
     // 2 = destination
 
+    // Create stack FILO to track path
+    static LinkedList<Position> path = new LinkedList<Position>();
+
     public static void main(String[] args) {
-//        int [][] data = {
-//                {4, 6, 3, 10},
-//                {4, 2, 40, 1, 7, 99, 100},
-//                {5, 34, 1, 43}
-//        };
-//        data[1][2] = 49;
-//        System.out.println(data[1][2]);
-//
-//        for (int i = 0; i < data.length; i++) {
-//            System.out.println("row index " + i);
-//            for (int j = 0; j < data[i].length; j++) {
-//                System.out.println(data[i][j]);
-//
-//            }
-//
-//        }
+
+        Position start = new Position(3, 0);
+        // Use push (beginning of list) vs add (end of list)
+        path.push(start);
+
+        // If a path is bad; set Position to zero
+
+        // Look at most recent Position in maze
+        // .peek = look, but do not remove
+        // NEED TO LOOK AT Y FIRST... ROW OF 2D ARRAY
+        // THEN X... COLUMN
+        assert path.peek() != null;
+        maze[path.peek().y][path.peek().x] = 0;
+
+
+
+
     }
 }
