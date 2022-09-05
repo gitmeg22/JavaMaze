58bbd2e44a59006d899ec9bb26f7ff1c04e1e073
gitmeg22 <35379612+gitmeg22@users.noreply.github.com>
2022-09-05 16:03:42 -0400

    Test easy-path movement through maze

--- JavaMaze/src/main/java/com/javamaze/MazeRunner.java
+++ JavaMaze/src/main/java/com/javamaze/MazeRunner.java
@@ -5,13 +5,18 @@ import java.util.LinkedList;
 
 public class MazeRunner {
     /* Store maze in program using static;
-    * Static makes it accessible to the main method;
-    * If maze was not static,
-    * would need instance of maze in main */
-    static int [][] maze = {
+     * Static makes it accessible to the main method;
+     * If maze was not static,
+     * would need instance of maze in main */
+    static int[][] maze = {
+//            {2, 0, 1, 1},
+//            {1, 1, 0, 1},
+//            {0, 1, 1, 1}
+
+            // Test Movement
             {2, 0, 1, 1},
-            {1, 1, 0, 1},
-            {0, 1, 1, 1}
+            {1, 1, 1, 0},
+            {0, 0, 0, 1}
     };
 
     // 0 = wall
@@ -27,18 +32,55 @@ public class MazeRunner {
         // Use push (beginning of list) vs add (end of list)
         path.push(start);
 
-        // If a path is bad; set Position to zero
+        while (true) {
+            // Look at most recent Position in maze
+            assert path.peek() != null;
+            int y = path.peek().y;
+            int x = path.peek().x;
 
-        // Look at most recent Position in maze
-        // .peek = look, but do not remove
-        // NEED TO LOOK AT Y FIRST... ROW OF 2D ARRAY
-        // THEN X... COLUMN
-        assert path.peek() != null;
-        int y = path.peek().y;
-        int x = path.peek().x;
+            // Mark path start as zero
+            maze[y][x] = 0;
 
-        // Mark path start as zero
-        maze[y][x] = 0;
+            // DOWN
+            if (maze[y + 1][x] == 2) {
+                System.out.println("Moved down - You WIN!");
+                return;
+            } else if (maze[y + 1][x] == 1) {
+                System.out.println("Moved down");
+                path.push(new Position(x, y + 1));
+                continue;
+            }
+
+            // LEFT
+            if (maze[y][x - 1] == 2) {
+                System.out.println("Moved left - You WIN!");
+                return;
+            } else if (maze[y][x - 1] == 1) {
+                System.out.println("Moved left");
+                path.push(new Position(x - 1, y));
+                continue;
+            }
+
+            // UP
+            if (maze[y - 1][x] == 2) {
+                System.out.println("Moved up - You WIN!");
+                return;
+            } else if (maze[y - 1][x] == 1) {
+                System.out.println("Moved up");
+                path.push(new Position(x, y - 1));
+                continue;
+            }
+
+            // RIGHT
+            if (maze[y][x + 1] == 2) {
+                System.out.println("Moved right - You WIN!");
+                return;
+            } else if (maze[y][x + 1] == 1) {
+                System.out.println("Moved right");
+                path.push(new Position(x + 1, y));
+                continue;
+            }
+        }
 
         // Check path going DOWN with ENHANCED SWITCH STATEMENT
       /*  switch (maze[y + 1][x]) {
@@ -47,17 +89,5 @@ public class MazeRunner {
             case 0 -> System.out.println("blocked");
             default -> System.out.println("NOTHING FOUND");
         }*/
-
-//        while (true) {
-            if (maze[y + 1][x] == 2) {
-                System.out.println("You WIN!");
-                return;
-            } else if (maze[y + 1][x] == 1) {
-                System.out.println("Moved down");
-                path.push(new Position(x, y + 1));
-            }
-//        }
-
-
     }
 }
