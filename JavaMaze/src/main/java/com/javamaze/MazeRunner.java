9f29aff135f16f411e88881ee0c93492a6bffd8c
gitmeg22 <35379612+gitmeg22@users.noreply.github.com>
2022-09-05 12:30:00 -0400

    Make maze 2d array static

--- JavaMaze/src/main/java/com/javamaze/MazeRunner.java
+++ JavaMaze/src/main/java/com/javamaze/MazeRunner.java
@@ -1,22 +1,36 @@
 package com.javamaze;
 
 public class MazeRunner {
+    /* Store maze in program using static;
+    * Static makes it accessible to the main method;
+    * If maze was not static,
+    * would need instance of maze in main */
+    int [][] maze = {
+            {2, 0, 1, 1},
+            {1, 1, 0, 1},
+            {0, 1, 1, 1}
+    };
+
+    // 0 = wall
+    // 1 = path
+    // 2 = destination
+
     public static void main(String[] args) {
-        int [][] data = {
-                {4, 6, 3, 10},
-                {4, 2, 40, 1, 7, 99, 100},
-                {5, 34, 1, 43}
-        };
-        data[1][2] = 49;
-        System.out.println(data[1][2]);
-
-        for (int i = 0; i < data.length; i++) {
-            System.out.println("row index " + i);
-            for (int j = 0; j < data[i].length; j++) {
-                System.out.println(data[i][j]);
-
-            }
-
-        }
+//        int [][] data = {
+//                {4, 6, 3, 10},
+//                {4, 2, 40, 1, 7, 99, 100},
+//                {5, 34, 1, 43}
+//        };
+//        data[1][2] = 49;
+//        System.out.println(data[1][2]);
+//
+//        for (int i = 0; i < data.length; i++) {
+//            System.out.println("row index " + i);
+//            for (int j = 0; j < data[i].length; j++) {
+//                System.out.println(data[i][j]);
+//
+//            }
+//
+//        }
     }
 }
